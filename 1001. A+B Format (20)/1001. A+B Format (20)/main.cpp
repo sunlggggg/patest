189d047be5ecbb7e1775d7c7979b76edab336f48
sunlggggg <2661377641@qq.com>
2017-03-05 23:44:57 +0800

    1001. A+B Format 20

---
+++ 1001. A+B Format (20)/1001. A+B Format (20)/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std; 
+
+int main(){
+
+	int a ; 
+	int b ; 
+	cin >> a ; 
+	cin >> b; 
+	int sum = a + b ; 
+	if(sum < 0 )
+		cout<<"-";
+	string strSum = to_string((long long )abs(sum) );
+	for(int i = 0 ; i < strSum.length(); i ++ ){
+		cout<<strSum[i];
+		if((strSum.length() - i ) % 3 == 1 && i != strSum.length() -1 ){
+			cout<<",";
+		}
+	}
+	return 0 ; 
+}
