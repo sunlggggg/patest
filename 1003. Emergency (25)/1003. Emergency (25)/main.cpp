ca3d5f8f2ae3d400c8796fdb64464dd120ca5dcb
sunlggggg <2661377641@qq.com>
2017-03-13 21:40:39 +0800

    1003 fininshed

--- 1003. Emergency (25)/1003. Emergency (25)/main.cpp
+++ 1003. Emergency (25)/1003. Emergency (25)/main.cpp
@@ -8,7 +8,15 @@ using namespace std;
 #define INF 0x3f3f3f3f
 
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+case 1: cout << vertexPath[C2] << " " << maxTeams[C2];
+	    C1 -> C2 or C2 - > C1 eew don't konw !
+case 2: if use the queue, maybe  the repetitive vertex will be added into the queue 
+        Vertex1 -> Vertex2 and Vertex3 but Vertex2 -> Vertex3 so the Vertex3 will be repetitive 
+
+
 input:
 5 6 0 2
 1 2 1 5 3
@@ -34,40 +42,44 @@ output:
 
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+const int MAXV = 510;
+int teamNumbers[MAXV];
+int map[MAXV][MAXV];
+int dist[MAXV];           // the distance from startVertex to EndVertex 
+int maxTeams[MAXV];       //the max teams arrive the 'i'
+int isFind[MAXV];       //the node wh 'out'
+int vertexPath[MAXV];   // the 'i' ways to arrive 
 
-int teamNumbers[1000];
-int map[1000][1000];
-int dist[1000];           // the distance from startVertex to EndVertex 
-int maxTeams[1000];       //the max teams arrive the 'i'
-int isFind[1000];       //the node wh 'out'
-int vertexPath[1000];   // the 'i' ways to arrive 
-
-void Dijkstra(int start , int numVertexs ){
+void Dijkstra(int start , int numVertexs ){ 
 	dist[start] = 0; // the distance to vertex 'start' is 0 
 	vertexPath[start] = 1;  //to vertex 'start' is 0 
-	maxTeams[start] = teamNumbers[start];
-	queue<int> q; 
-	q.push(start);
-	while (!q.empty()){
-		int vertex = q.front();
-		q.pop();
+	maxTeams[start] = teamNumbers[start]; 
+	for (int j = 0; j < numVertexs; j++ ){     
+		int  vertex = -1, MIN = INF;
 		for (int i = 0; i < numVertexs; i++){
-			if (map[vertex][i] != INF && isFind[i] == 0 ){
-				if (dist[vertex] + map[vertex][i] < dist[i]){
-					dist[i] = dist[vertex] + map[vertex][i];
-					vertexPath[i] = vertexPath[vertex];
-					maxTeams[i] = maxTeams[vertex] + teamNumbers[i]; 
-				}
-				else if (dist[vertex] + map[vertex][i] == dist[i]){
-					vertexPath[i] = vertexPath[vertex] + vertexPath[i];
-					if (maxTeams[vertex] + teamNumbers[i] > maxTeams[i]){
-						maxTeams[i] = maxTeams[vertex] + teamNumbers[i];
-					}
-				}
-				q.push(i);
+			if (isFind[i] == 0 && dist[i] < MIN){
+				vertex = i;
+				MIN = dist[i];
 			}
 		}
+		if (vertex == -1)
+			return;
 		isFind[vertex] = 1;
+		for (int v = 0; v < numVertexs; v++){
+			if (map[vertex][v] != INF && isFind[v] == 0){
+				if (dist[vertex] + map[vertex][v] < dist[v]){
+					dist[v] = dist[vertex] + map[vertex][v];
+					vertexPath[v] = vertexPath[vertex];
+					maxTeams[v] = maxTeams[vertex] + teamNumbers[v];
+				}
+				else if (dist[vertex] + map[vertex][v] == dist[v]){
+					vertexPath[v] = vertexPath[vertex] + vertexPath[v];
+					if (maxTeams[vertex] + teamNumbers[v] > maxTeams[v]){
+						maxTeams[v] = maxTeams[vertex] + teamNumbers[v];
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -77,15 +89,17 @@ int main(){
 	cin >> N >> M >> C1 >> C2;
 	for (int i = 0; i < N; i++)
 		cin >> teamNumbers[i];
-	memset(dist, INF, sizeof(dist));
-	memset(map, INF, sizeof(map));
+	fill(dist, dist + MAXV,INF);
+	fill(map[0], map[0] + MAXV * MAXV , INF);
+	//memset(dist, INF, sizeof(dist));
+	//memset(map, INF, sizeof(map));
 	memset(vertexPath, 1, sizeof(vertexPath));
 	memset(isFind, 0, sizeof(isFind));
 	memset(maxTeams, 0,sizeof(maxTeams));
 	for (int i = 0; i < M ; i ++ ){
 		int c1, c2, L;
 		cin >> c1 >> c2 >> L;
-		map[c1][c2] = L;
+		map[c1][c2] = map[c2][c1]  = L; 
 	}
 	Dijkstra(C1,N);
 	cout << vertexPath[C2] <<" "<< maxTeams[C2];
